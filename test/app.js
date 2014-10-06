9ff65f4fbefb24baeece699f7319c0b8e08c9c9f
wanderer <mjbecze@gmail.com>
2014-10-06 17:56:14 -0400

    changed location of defaults

--- test/app.js
+++ test/app.js
@@ -1,13 +1,53 @@
-var App = require('../');
+var App = require('../'),
+  fs = require('fs'),
+  async = require('async');
 
-var app = new App();
+var app;
 
 describe('basic app functions', function () {
   it('should start', function (done) {
+    app = new App();
     app.start(done);
   });
 
-  it('should stop', function(done) {
-    app.stop(done); 
+  it('should stop', function (done) {
+    app.stop(done);
+  });
+
+  it.only('should connect to another peer', function (done) {
+    var count = 0,
+      peers = [];
+
+    async.whilst(
+      function () {
+        return count < 2;
+      },
+      function (callback) {
+        var settings = {
+          'network': {
+            'port': 30316,
+            'host': '0.0.0.0'
+          },
+          'upnp': false,
+          'rpc': false
+        };
+
+        count++;
+
+        settings.network.port += count;
+        console.log(settings.network.port);
+        settings.path = './test/testClient' + count;
+
+        try{
+          fs.mkdirSync(settings.path);
+        }catch(e){}
+
+        var app = new App(settings);
+        peers.push(app);
+        app.start(callback);
+
+      },
+      done
+    );
   });
 });
--- test/rpc.js
+++ test/rpc.js
@@ -18,4 +18,3 @@ app.start(function () {
 
   });
 });
-
