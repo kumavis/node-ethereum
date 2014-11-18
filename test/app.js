d0edd6757b491b6caec55f7166055cd4908f7c78
wanderer <mjbecze@gmail.com>
2014-11-17 20:40:57 -0500

    added test for network

--- test/app.js
+++ test/app.js
@@ -2,33 +2,35 @@ var App = require('../'),
   fs = require('fs'),
   async = require('async');
 
-var app;
+var app,
+  peers = [],
+  numberOfPeers = 5, //the amount of peers to create in this test must be more than 3 for the tests to work properly
+  startPort = 30316;
 
-describe('basic app functions', function () {
+describe('basic app functions', function() {
 
-  it('should start', function (done) {
+  it('should start', function(done) {
     app = new App();
     app.start(done);
   });
 
-  it('should stop', function (done) {
+  it('should stop', function(done) {
     app.stop(done);
   });
 
-  it('should connect to another peer', function (done) {
-    var count = 0,
-      peers = [];
+  it('should start serveral instances', function(done) {
+    var count = 0;
 
     async.whilst(
 
-      function () {
+      function() {
         return count < 5;
       },
 
-      function (callback) {
+      function(callback) {
         var settings = {
           'network': {
-            'port': 30316,
+            'port': startPort,
             'host': '0.0.0.0'
           },
           'upnp': false,
@@ -36,13 +38,12 @@ describe('basic app functions', function () {
         };
 
         count++;
-
         settings.network.port += count;
         settings.path = './test/testClient' + count;
 
-        try{
+        try {
           fs.mkdirSync(settings.path);
-        }catch(e){}
+        } catch (e) {}
 
         var app = new App(settings);
         peers.push(app);
@@ -50,9 +51,18 @@ describe('basic app functions', function () {
 
       },
 
-      function(){
+      function() {
         done();
       }
     );
+  });
+
+  it('two peers should connect to each other', function(done) {
+
+    peers[1].network.on('message.hello', function() {
+      done();
+    });
+    peers[0].network.connect(startPort + 2, '0.0.0.0');
+
   });
 });
